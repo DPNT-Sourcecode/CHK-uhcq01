d7cae3c01f75bdfd73feeb29edc5129f7d2bbfd2
sbx_user1051 <sbx_user1051@169.254.89.149>
2024-09-23 18:23:55 +0000

    Mon Sep 23 18:23:55 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from math import floor
+from collections import Counter
 import re
 
 # noinspection PyUnusedLocal
@@ -17,10 +18,10 @@ def checkout(skus: str) -> int:
     items = ['A','B','C','D','E']
 
     pricings = pd.DataFrame({
-        'Item': ['A', 'A','B','C','D','E'],
+        'Item': ['A', 'A', 'B', 'C', 'D', 'E'],
         'Price': [50, 50, 30, 20, 15],
         'Special offer count': [3, 5, 2, None, None, 2],
-        'Special offer price': [130, 200, 45, None, None, 0]
+        'Special offer price': [130, 200, 45, None, None, '-B']
     })
 
     if not isinstance(skus, str):
@@ -30,13 +31,10 @@ def checkout(skus: str) -> int:
     if len(filtered_skus) < len(skus):
         return -1 # Invalid characters were present
     
-    
-
-
     total = 0
 
-    for i, row in pricings.iterrows():
-        n = filtered_skus.count(row['Item']) # Occurrences of the item in the basket
+    for item in items:
+        n = filtered_skus.count(item) # Occurrences of the item in the basket
 
         if row['Special offer count'] is not None and n >= row['Special offer count']:
             offer_count = floor(n / row['Special offer count'])
@@ -50,7 +48,21 @@ def checkout(skus: str) -> int:
     return int(total)
             
     
+def checkout(skus: str) -> int:
 
+    if not isinstance(skus, str):
+        return -1 # Input was not a string
+    
+    filtered_skus = re.sub(r'[^ABCD]', '', skus)
+    if len(filtered_skus) < len(skus):
+        return -1 # Invalid characters were present
+    
+    total = 0
+
+    item_counts = Counter(filtered_skus)
+
+    if 'E'
+    
 
 print(checkout('ABCD'))
 print(checkout('AAA'))
@@ -58,5 +70,6 @@ print(checkout('BBBBB'))
 print(checkout('AAAAAA'))
 print(checkout('a'))
 print(checkout('-'))
+
 
 
