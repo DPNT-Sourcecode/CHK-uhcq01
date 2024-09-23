d7d47fc7a7936ac33c45db7061ad9e310a7511d7
sbx_user1051 <sbx_user1051@169.254.89.149>
2024-09-23 17:54:59 +0000

    Mon Sep 23 17:54:59 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,10 +1,18 @@
-
+import pandas as pd
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
 
+    pricings = pd.DataFrame({
+        'Item': ['A','B','C','D'],
+        'Price': [50, 30, 20, 15],
+        'Special offers': ['3A for 130', '2B for 45', None, None]
+    })
+
+    print(pricings)
+
     if not isinstance(skus, str):
         return -1
     
-    
+checkout('sdas')
