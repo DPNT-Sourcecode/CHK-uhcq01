0bbbd5995b310b7f8b81db15473afb66ffb32b09
sbx_user1051 <sbx_user1051@169.254.89.149>
2024-09-23 19:05:22 +0000

    Mon Sep 23 19:05:22 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,9 +9,9 @@ import re
 def checkout(skus: str) -> int:
 
     pricings = pd.DataFrame({
-        'Item': ['A', 'A', 'A', 'B', 'B', 'C', 'D', 'E'],
-        'Special offer count': [5, 3, 1, 2, 1, 1, 1, 1],
-        'Special offer price': [200, 130, 50, 45, 30, 20, 15, 40]
+        'Item': ['A', 'A', 'A', 'B', 'B', 'C', 'D', 'E', 'F'],
+        'Special offer count': [5, 3, 1, 2, 1, 1, 1, 1, 1],
+        'Special offer price': [200, 130, 50, 45, 30, 20, 15, 40, 10]
     })
 
     if not isinstance(skus, str):
@@ -22,8 +22,13 @@ def checkout(skus: str) -> int:
         return -1 # Invalid characters were present
     
     item_counter = Counter(filtered_skus)
+    print(item_counter)
     if item_counter['B'] > 0:
         item_counter['B'] -= floor(item_counter['E'] / 2)
+
+    if item_counter['F'] > 2:
+        item_counter['F'] -= floor(item_counter['F'] / 2)
+    print(item_counter)
     
     total = 0
 
@@ -36,16 +41,18 @@ def checkout(skus: str) -> int:
             total += floor(n / offer) * pricings[(pricings['Item'] == item) & (pricings['Special offer count'] == offer)]['Special offer price'].values[0]
             n = n % offer
 
+    print(total)
     return int(total)
             
     
 
-# assert checkout('ABCD') == 115
-# assert checkout('AAA') == 130
-# assert checkout('AAAAA') == 200
-# assert checkout('EE') == 80
-# assert checkout('a') == -1
-# assert checkout('-') == -1
-# assert checkout('EEB') == 80
-# assert checkout('EEBB') == 110
-# assert checkout('EEEEEBBB') == 230
+assert checkout('ABCD') == 115
+assert checkout('AAA') == 130
+assert checkout('AAAAA') == 200
+assert checkout('EE') == 80
+assert checkout('a') == -1
+assert checkout('-') == -1
+assert checkout('EEB') == 80
+assert checkout('EEBB') == 110
+assert checkout('EEEEEBBB') == 230
+assert checkout('EEEEEBBBFF') == 240
