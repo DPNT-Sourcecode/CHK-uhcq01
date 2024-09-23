803ec5536cecf77b507a94c3a2daa7dd4de89c94
sbx_user1051 <sbx_user1051@169.254.89.149>
2024-09-23 17:44:24 +0000

    Mon Sep 23 17:44:24 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,6 +1,22 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x: int, y: int) -> int:
 
-    raise NotImplementedError()
+    if not isinstance(x, int):
+        raise Exception('x must be of type integer')
+    
+    if not isinstance(y, int):
+        raise Exception('y must be of type integer')
+
+    if x >= 0 and x <= 100 and y >= 0 and y <= 100:
+        return x + y
+    else:
+        raise Exception('x or y not in range 0 - 100')
+    
+
+
+print(compute(1, 2))
+print(compute(0, 2))
+print(compute(-1, 2))
+print(compute('a', 1))
 
 
