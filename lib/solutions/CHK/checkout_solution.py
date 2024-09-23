6f865cbed3f537b4e0d6db32e2f5b748f69239a8
sbx_user1051 <sbx_user1051@169.254.89.149>
2024-09-23 18:17:55 +0000

    Mon Sep 23 18:17:55 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,6 +6,13 @@ import re
 # skus = unicode string
 def checkout(skus: str) -> int:
 
+    # pricings = pd.DataFrame({
+    #     'Item': ['A','B','C','D'],
+    #     'Price': [50, 30, 20, 15],
+    #     'Special offer count': [3, 2, None, None],
+    #     'Special offer price': [130, 45, None, None]
+    # })
+
     pricings = pd.DataFrame({
         'Item': ['A','B','C','D'],
         'Price': [50, 30, 20, 15],
@@ -21,16 +28,16 @@ def checkout(skus: str) -> int:
         return -1 # Invalid characters were present
     
     
+
+    
     total = 0
 
     for i, row in pricings.iterrows():
-        
         n = filtered_skus.count(row['Item']) # Occurrences of the item in the basket
 
         if row['Special offer count'] is not None and n >= row['Special offer count']:
             offer_count = floor(n / row['Special offer count'])
             remainder = n - (row['Special offer count'] * offer_count)
-
             total += offer_count * row['Special offer price']
             total += remainder * row['Price']
 
@@ -40,9 +47,12 @@ def checkout(skus: str) -> int:
     return int(total)
             
     
+
+
 print(checkout('ABCD'))
 print(checkout('AAA'))
 print(checkout('BBBBB'))
 print(checkout('AAAAAA'))
 print(checkout('a'))
 print(checkout('-'))
+
